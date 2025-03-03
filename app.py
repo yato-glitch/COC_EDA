fbb8fe35f2e1b92b282645070cc9c6e99c3aa65a
Yash Bhardwaj <bhardwaj.yash8700@gmail.com>
2025-03-03 22:20:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def main():
    
     col1, col2 = st.columns([1, 1])
     with col1:
-        st.image("https://github.com/yato-glitch/COC_EDA/blob/main/857565.jpg", width=450)
+        st.image("https://github.com/yato-glitch/COC_EDA/raw/main/857565.jpg", width=450)
+
     with col2:
-        st.image("https://github.com/yato-glitch/COC_EDA/blob/main/782659.jpg", width=450)
+        st.image("https://github.com/yato-glitch/COC_EDA/raw/main/782659.jpg", width=450)
     
     st.markdown("""
         <h1 style='text-align: center; color: white;'>Clash of Clans Player Analysis & Strategy Recommendations</h1>
